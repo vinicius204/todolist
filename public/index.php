a3748f4e2376473ecc519930e1f30d5bef036daf
Vini <vinicius.gabriel@duque.g12.br>
2025-04-10 19:53:00 -0300

    Primeiro commit: Configuração da index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,18 @@
 <?php
+ini_set("display_errors", 1);
+ini_set("display_startup_errors", 1);
+error_reporting(E_ALL);
+
 // Inicia a sessão no navegador
+session_start();
 
 // Insira aqui os includes e/ou requires
-
+require_once __DIR__."/../src/controllers/controll.php"
+echo(__DIR__)
 // Processa as requisições do navegador
-
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    handleRequest();
+}
 //exibir a lista de tarefas
+showTasks();
 ?>
